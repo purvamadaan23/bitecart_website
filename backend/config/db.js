29f9b93a55dacec14fc2ccc2b67b4ef0e7a59074
purva <purvamadaan418@gmail.com>
2024-10-01 23:24:58 +0530

    changing configuration file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
-  await mongoose.connect('mongodb+srv://vinit:87654321@cluster0.3dbj6uk.mongodb.net/zomato').then(()=>console.log("DB connected"))
+  await mongoose.connect('mongodb+srv://purvamadaan418:BBiwox1D26ijfvep@cluster0.r38eh.mongodb.net/biteCart').then(()=>console.log("DB connected"))
 }
